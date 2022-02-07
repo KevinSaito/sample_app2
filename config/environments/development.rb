60bc8e2e2b6a1fcf822a782ae89167638007df00
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-07 01:15:20 +0000

    [Update]Listモデル追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "f08832ae69a84c7c8047938d67265aec.vfs.cloud9.us-east-1.amazonaws.com"
 end
