170b97151f20fb56f3e36bb0e84cae6ffc2a1677
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-08 01:38:15 +0000

    [Update]Rails c 試用

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,7 @@ class ListsController < ApplicationController
       redirect_to list_path(@list.id)
     else
       render :new
+    end
   end
 
   def index
@@ -22,13 +23,13 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
